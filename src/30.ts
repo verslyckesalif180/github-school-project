1357d820f077f7675d118f916b775fd91ae62387
verslyckesalif180 <verslyckesalif180@gmail.com>
2025-04-24 15:17:37 +0000

    Add 30.ts

---
+++ src/30.ts
@@ -0,0 +1,10 @@
+// Example: Random TypeScript Code with Comments
+
+let x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(`current value of i is ${i}`);
+}
